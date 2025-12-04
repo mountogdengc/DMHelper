b5ce389c894d62b7bf143d4c81e6a90e1dfe256e
DM Helper <44725987+dm-helper@users.noreply.github.com>
2025-12-04 03:54:30 +0100

    Improving the performance of the filter

--- DMHelper/src/mapmanagerdialog.cpp
+++ DMHelper/src/mapmanagerdialog.cpp
@@ -4,7 +4,6 @@
 #include "dmhcache.h"
 #include "dmconstants.h"
 #include <QStandardItemModel>
-#include <QSortFilterProxyModel>
 #include <QFileDialog>
 #include <QDir>
 #include <QImageReader>
@@ -13,6 +12,7 @@
 #include <QScrollArea>
 #include <QDomDocument>
 #include <QDomElement>
+#include <QListWidget>
 
 const int MAPMANAGERDIALOG_CACHE_IMAGE_SIZE = 256;
 const int MAPMANAGERDIALOG_METADATA = Qt::UserRole + 1;
@@ -29,10 +29,11 @@ MapManagerDialog::MapManagerDialog(OptionsContainer& options, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::MapManagerDialog),
     _model(new QStandardItemModel()),
-    _proxy(new QSortFilterProxyModel()),
+    _proxy(new TagFilterProxyModel()),
     _options(options),
     _currentPath(),
-    _searchList()
+    _searchList(),
+    _tagList()
 {
     ui->setupUi(this);
 
@@ -45,6 +46,9 @@ MapManagerDialog::MapManagerDialog(OptionsContainer& options, QWidget *parent) :
     connect(ui->btnRefresh, &QPushButton::clicked, this, &MapManagerDialog::findMaps);
     connect(ui->treeView->selectionModel(), &QItemSelectionModel::selectionChanged, this, &MapManagerDialog::selectItem);
     connect(ui->treeView, &QTreeView::doubleClicked, this, &MapManagerDialog::openPreviewDialog);
+    connect(ui->btnAutoTag, &QPushButton::clicked, this, &MapManagerDialog::addTags);
+    connect(ui->btnBrowseTags, &QPushButton::clicked, this, &MapManagerDialog::browseTags);
+    connect(ui->edtSearch, &QLineEdit::editingFinished, this, &MapManagerDialog::handleTagsEdited);
 }
 
 MapManagerDialog::~MapManagerDialog()
@@ -64,6 +68,10 @@ void MapManagerDialog::showEvent(QShowEvent *event)
 void MapManagerDialog::selectItem(const QItemSelection &current, const QItemSelection &previous)
 {
     Q_UNUSED(previous);
+
+    if(current.indexes().count() == 0)
+        return;
+
     QStandardItem* currentItem = _model->itemFromIndex(_proxy->mapToSource(current.indexes().first()));
     if(currentItem)
     {
@@ -71,6 +79,8 @@ void MapManagerDialog::selectItem(const QItemSelection &current, const QItemSele
         if(variantData.isValid())
         {
             MapFileMetadata metaData = variantData.value<MapFileMetadata>();
+            ui->edtTags->setText(metaData._tags.join(QChar::Space));
+
             if(metaData._type != DMHelper::FileType_Directory)
             {
                 QPixmap itemPixmap;
@@ -166,7 +176,6 @@ void MapManagerDialog::findMaps()
 
     _currentPath = ui->edtMapPath->text();
 
-    _model->clear();
     _searchList.clear();
     _searchList.append(QPair<QStandardItem*, const QString&>(_model->invisibleRootItem(), ui->edtMapPath->text()));
     QTimer::singleShot(0, this, &MapManagerDialog::scanNextDirectory);
@@ -201,21 +210,152 @@ void MapManagerDialog::scanDirectory(QStandardItem* parent, const QString& absol
 
     for(const QFileInfo &entry : dirEntries)
     {
-        QStandardItem* newItem = new QStandardItem(entry.fileName());
-        newItem->setEditable(false);
-        MapFileMetadata metaData{ DMHelper::FileType_Unknown, entry.absoluteFilePath(), QStringList() };
+        QString absoluteFilePath = entry.absoluteFilePath();
+        QStandardItem* entryItem = containsEntry(*parent, absoluteFilePath);
+        if(!entryItem)
+        {
+            QStandardItem* newItem = new QStandardItem(entry.fileName());
+            newItem->setEditable(false);
+            MapFileMetadata metaData{ DMHelper::FileType_Unknown, absoluteFilePath, QStringList() };
+            if(entry.isDir())
+                metaData._type = DMHelper::FileType_Directory;
+            metaData._tags = proposeTags(absoluteFilePath);
+
+            newItem->setData(QVariant::fromValue(metaData), MAPMANAGERDIALOG_METADATA);
+            parent->appendRow(newItem);
+        }
+
         if(entry.isDir())
+            _searchList.append(QPair<QStandardItem*, QString>(entryItem, absoluteFilePath));
+    }
+}
+
+void MapManagerDialog::addTags()
+{
+    if(!_model)
+        return;
+
+    for(int i = 0; i < _model->rowCount(); ++i)
+    {
+        if(_model->item(i))
+            addTagsToItem(*_model->item(i));
+    }
+
+    // Update the tags text for the currently selected item
+    if(!ui->treeView->selectionModel()->selection().isEmpty())
+    {
+        QStandardItem* currentItem = _model->itemFromIndex(_proxy->mapToSource(ui->treeView->selectionModel()->selection().indexes().first()));
+        if(currentItem)
         {
-            metaData._type = DMHelper::FileType_Directory;
-            _searchList.append(QPair<QStandardItem*, QString>(newItem, entry.absoluteFilePath()));
+            QVariant variantData = currentItem->data(MAPMANAGERDIALOG_METADATA);
+            if(variantData.isValid())
+            {
+                MapFileMetadata metaData = variantData.value<MapFileMetadata>();
+                ui->edtTags->setText(metaData._tags.join(QChar::Space));
+            }
         }
-        else if(entry.isFile())
+    }
+
+    QTimer::singleShot(0, this, &MapManagerDialog::writeModel);
+}
+
+void MapManagerDialog::addTagsToItem(QStandardItem& item)
+{
+    QVariant variantData = item.data(MAPMANAGERDIALOG_METADATA);
+    if(!variantData.isValid())
+        return;
+
+    MapFileMetadata metaData = variantData.value<MapFileMetadata>();
+    if((metaData._type != DMHelper::FileType_Directory) && (!metaData._filePath.isEmpty()))
+    {
+        QFileInfo fileInfo(metaData._filePath);
+        QStringList newTags = proposeTags(fileInfo.fileName());
+        if(metaData._tags.isEmpty())
         {
-            //if(QImageReader(entry.absoluteFilePath()).canRead())
+            metaData._tags = newTags;
+            registerTags(newTags);
         }
-        newItem->setData(QVariant::fromValue(metaData), MAPMANAGERDIALOG_METADATA);
-        parent->appendRow(newItem);
-    }    
+        else
+        {
+            for(QString& tagString : newTags)
+            {
+                if(!metaData._tags.contains(tagString))
+                {
+                    metaData._tags.append(tagString);
+                    registerTag(tagString);
+                }
+            }
+        }
+
+        if(!metaData._tags.isEmpty())
+            item.setData(QVariant::fromValue(metaData), MAPMANAGERDIALOG_METADATA);
+    }
+
+    for(int i = 0; i < _model->rowCount(); ++i)
+    {
+        if(item.child(i))
+            addTagsToItem(*item.child(i));
+    }
+}
+
+void MapManagerDialog::browseTags()
+{
+    if((_tagList.isEmpty()) || (!_proxy))
+        return;
+
+    QDialog* dlgTags = new QDialog(this);
+
+    QVBoxLayout* vLayout = new QVBoxLayout();
+
+    QListWidget* listWidget = new QListWidget();
+    listWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+    QStringList tags = _tagList.values();
+    tags.sort(Qt::CaseInsensitive);
+    for(QString tag : tags)
+    {
+        QListWidgetItem* newItem = new QListWidgetItem(tag);
+        listWidget->addItem(newItem);
+        if(_proxy->getRequiredTags().contains(tag))
+            newItem->setSelected(true);
+    }
+    vLayout->addWidget(listWidget);
+
+    QHBoxLayout* hLayout = new QHBoxLayout();
+    QPushButton* okButton = new QPushButton(QString("OK"));
+    hLayout->addWidget(okButton);
+    connect(okButton, &QPushButton::clicked, dlgTags, &QDialog::accept);
+    QPushButton* cancelButton = new QPushButton(QString("Cancel"));
+    hLayout->addWidget(cancelButton);
+    connect(cancelButton, &QPushButton::clicked, dlgTags, &QDialog::reject);
+    hLayout->insertStretch(0,10);
+    QPushButton* clearButton = new QPushButton(QString("Clear"));
+    hLayout->insertWidget(0, clearButton);
+    connect(clearButton, &QPushButton::clicked, listWidget, &QListWidget::clearSelection);
+    vLayout->addLayout(hLayout);
+
+    dlgTags->setLayout(vLayout);
+    if(dlgTags->exec() == QDialog::Accepted)
+    {
+        QStringList selectedTags;
+        QList<QListWidgetItem*> selectedItems = listWidget->selectedItems();
+        for(QListWidgetItem* item : selectedItems)
+        {
+            selectedTags.append(item->text());
+        }
+        _proxy->setRequiredTags(selectedTags);
+        ui->edtSearch->setText(selectedTags.join(QChar::Space));
+    }
+
+    dlgTags->deleteLater();
+}
+
+void MapManagerDialog::handleTagsEdited()
+{
+    if((!_proxy) || (ui->edtSearch->text().isEmpty()))
+        return;
+
+    QStringList editTags = ui->edtSearch->text().split(QChar::Space, Qt::SkipEmptyParts);
+    _proxy->setRequiredTags(editTags);
 }
 
 void MapManagerDialog::readModel()
@@ -223,8 +363,7 @@ void MapManagerDialog::readModel()
     if((!_model) || (!_model->invisibleRootItem()) || (_currentPath.isEmpty()))
         return;
 
-    _searchList.clear();
-    _model->clear();
+    clearModel();
 
     QDomDocument doc("DMHelperXML");
     QString modelFile = DMHCache().getCacheFilePath(_currentPath, QString("xml"));
@@ -300,6 +439,35 @@ void MapManagerDialog::writeModel()
     qDebug() << "[MapManagerDialog] Map manager cache file written: " << modelFile;
 }
 
+void MapManagerDialog::clearModel()
+{
+    _searchList.clear();
+    _model->clear();
+    _tagList.clear();
+}
+
+QStandardItem* MapManagerDialog::containsEntry(QStandardItem& item, const QString& fullPath)
+{
+    if(item.hasChildren())
+    {
+        for(int i = 0; i < item.rowCount(); ++i)
+        {
+            if(item.child(i))
+            {
+                QVariant variantData = item.child(i)->data(MAPMANAGERDIALOG_METADATA);
+                if(variantData.isValid())
+                {
+                    MapFileMetadata metaData = variantData.value<MapFileMetadata>();
+                    if(metaData._filePath == fullPath)
+                        return item.child(i);
+                }
+            }
+        }
+    }
+
+    return nullptr;
+}
+
 void MapManagerDialog::setCurrentPath(const QString& path)
 {
     if(_currentPath == path)
@@ -325,7 +493,10 @@ void MapManagerDialog::inputItemXML(QDomElement &element, QStandardItem& parent)
     if(element.hasAttribute("type"))
         metaData._type = static_cast<DMHelper::FileType>(element.attribute("type").toInt());
     if(element.hasAttribute("tags"))
+    {
         metaData._tags = element.attribute("tags").split(";", Qt::SkipEmptyParts);
+        registerTags(metaData._tags);
+    }
     newItem->setData(QVariant::fromValue(metaData), MAPMANAGERDIALOG_METADATA);
 
     parent.appendRow(newItem);
@@ -365,4 +536,119 @@ void MapManagerDialog::outputItemXML(QDomDocument &doc, QDomElement &parent, QSt
     }
 
     parent.appendChild(element);
+}
+
+void MapManagerDialog::registerTag(const QString& tag)
+{
+    _tagList.insert(tag);
+}
+
+void MapManagerDialog::registerTags(const QStringList& tags)
+{
+    for(const QString& tagString : tags)
+    {
+        registerTag(tagString);
+    }
+}
+
+QStringList MapManagerDialog::proposeTags(const QString &filename) const
+{
+    QSet<QString> tags;
+
+    QFileInfo info(filename);
+    QString base = info.completeBaseName().toLower();
+    QString ext  = info.suffix().toLower();
+
+    // --------- NORMALIZE ----------
+    QString cleaned = base;
+    cleaned.replace("_", " ");
+    cleaned.replace("-", " ");
+    cleaned.replace("[", " ");
+    cleaned.replace("]", " ");
+    cleaned.replace("(", " ");
+    cleaned.replace(")", " ");
+    cleaned.replace(",", " ");
+    cleaned.replace(".", " ");
+
+    QStringList tokens = cleaned.split(" ", Qt::SkipEmptyParts);
+
+    // --------- CREATOR PREFIX MATCHING ----------
+    for(const auto &pair : creators)
+    {
+        if (filename.startsWith(pair.first, Qt::CaseInsensitive))
+            tags.insert(pair.second);
+    }
+
+    // --------- KEYWORD MATCHING ----------
+    for(const QString &token : tokens)
+    {
+        if(keywordToTags.contains(token))
+        {
+            for (const QString &tag : keywordToTags[token])
+                tags.insert(tag);
+        }
+
+        if(timeTags.contains(token))
+            tags.insert(timeTags[token]);
+
+        if(gridWords.contains(token))
+            tags.insert(gridWords[token]);
+    }
+
+    // --------- EXTENSION MATCHING ----------
+    if(extensionTags.contains(ext))
+        tags.insert(extensionTags[ext]);
+
+    return tags.values();
+}
+
+QStringList MapManagerDialog::TagFilterProxyModel::getRequiredTags()
+{
+    return _requiredTags;
+}
+
+void MapManagerDialog::TagFilterProxyModel::setRequiredTags(const QStringList& tags)
+{
+    _requiredTags = tags;
+    invalidateFilter();
+}
+
+void MapManagerDialog::TagFilterProxyModel::clearRequiredTags()
+{
+    _requiredTags.clear();
+    invalidateFilter();
+}
+
+bool MapManagerDialog::TagFilterProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
+{
+    if(_requiredTags.isEmpty())
+        return true;
+
+    QModelIndex index = sourceModel()->index(source_row, 0, source_parent);
+    QVariant variantData = index.data(MAPMANAGERDIALOG_METADATA);
+    if(!variantData.isValid())
+        return true;
+
+    MapFileMetadata metaData = variantData.value<MapFileMetadata>();
+
+    // If any required tag matches → accept row
+    if(listContainsAllTags(metaData._tags))
+        return true;
+
+    // Check the children
+    int childCount = sourceModel()->rowCount(index);
+    for(int i = 0; i < childCount; ++i)
+        if(filterAcceptsRow(i, index))
+            return true;
+
+    // Reject
+    return false;
+}
+
+bool MapManagerDialog::TagFilterProxyModel::listContainsAllTags(const QStringList& list) const
+{
+    for(const QString& r : _requiredTags)
+        if(!list.contains(r))
+            return false;
+    return true;
 }
--- DMHelper/src/mapmanagerdialog.h
+++ DMHelper/src/mapmanagerdialog.h
@@ -2,6 +2,7 @@
 #define MAPMANAGERDIALOG_H
 
 #include <QDialog>
+#include <QSortFilterProxyModel>
 
 namespace Ui {
 class MapManagerDialog;
@@ -9,7 +10,6 @@ class MapManagerDialog;
 
 class OptionsContainer;
 class QStandardItemModel;
-class QSortFilterProxyModel;
 class QStandardItem;
 class QItemSelection;
 class QDomDocument;
@@ -41,21 +41,168 @@ protected slots:
     void scanNextDirectory();
     void scanDirectory(QStandardItem* parent, const QString& absolutePath);
 
+    void addTags();
+    void addTagsToItem(QStandardItem& item);
+    void browseTags();
+    void handleTagsEdited();
+
     void readModel();
     void writeModel();
 
 private:
+    void clearModel();
+    QStandardItem* containsEntry(QStandardItem& item, const QString& fullPath);
     void setCurrentPath(const QString& path);
     void inputItemXML(QDomElement &element, QStandardItem& parent);
     void outputItemXML(QDomDocument &doc, QDomElement &parent, QStandardItem& item);
 
+    void registerTag(const QString& tag);
+    void registerTags(const QStringList& tags);
+
+    QStringList proposeTags(const QString &filename) const;
+
+    class TagFilterProxyModel;
+
     Ui::MapManagerDialog *ui;
 
     QStandardItemModel* _model;
-    QSortFilterProxyModel* _proxy;
+    TagFilterProxyModel* _proxy;
     OptionsContainer& _options;
     QString _currentPath;
     QList<QPair<QStandardItem*, QString>> _searchList;
+    QSet<QString> _tagList;
+
+    // ------------------ KEYWORD → TAGS ------------------
+    const QHash<QString, QStringList> keywordToTags {
+        // Environments
+        {"cave", {"cave", "dungeon"}},
+        {"cavern", {"cave"}},
+        {"forest", {"forest", "outdoor"}},
+        {"swamp", {"swamp", "outdoor"}},
+        {"desert", {"desert", "outdoor"}},
+        {"arctic", {"snow", "outdoor"}},
+        {"snow", {"snow", "outdoor"}},
+        {"mountain", {"mountain", "outdoor"}},
+        {"waterfall", {"waterfall", "water"}},
+        {"river", {"river", "water"}},
+        {"lake", {"lake", "water"}},
+        {"coast", {"coast", "water"}},
+        {"shore", {"shore", "water"}},
+        {"reef", {"reef", "water"}},
+
+        // Urban
+        {"town", {"town", "settlement"}},
+        {"village", {"village", "settlement"}},
+        {"city", {"city", "urban"}},
+        {"market", {"market", "urban"}},
+        {"gate", {"gate", "urban"}},
+        {"tower", {"tower", "building"}},
+        {"castle", {"castle", "fortification"}},
+        {"palace", {"palace", "urban"}},
+
+        // Dungeons / ruins
+        {"crypt", {"crypt", "dungeon"}},
+        {"catacombs", {"catacombs", "dungeon"}},
+        {"ruins", {"ruins"}},
+        {"sewer", {"sewer", "underground"}},
+        {"mine", {"mine", "underground"}},
+
+        // Thematic
+        {"lava", {"lava", "hazard"}},
+        {"infernal", {"hell", "infernal"}},
+        {"hell", {"hell", "infernal"}},
+        {"haunted", {"haunted"}},
+        {"spooky", {"haunted"}},
+
+        // Locations / modules
+        {"barovia", {"barovia", "curseofstrahd"}},
+        {"ravenloft", {"ravenloft", "curseofstrahd"}},
+        {"phandalin", {"phandalin", "lmop"}},
+        {"cragmaw", {"cragmaw", "lmop"}},
+        {"skullport", {"skullport", "underdark"}},
+        {"velkynvelve", {"velkynvelve", "underdark"}},
+        {"darklake", {"darklake", "underdark"}},
+
+        // Misc gameplay locations
+        {"arena", {"arena"}},
+        {"camp", {"camp"}},
+        {"bridge", {"bridge"}},
+        {"airship", {"airship", "vehicle"}},
+        {"ship", {"ship", "vehicle"}},
+        {"nautiloid", {"nautiloid", "spelljammer"}}
+    };
+
+    // ------------------ TIME TAGS ------------------
+    const QHash<QString, QString> timeTags {
+        {"day", "day"},
+        {"light", "day"},
+        {"night", "night"},
+        {"dark", "night"},
+        {"dawn", "dawn"},
+        {"dusk", "dusk"},
+        {"torchlight", "torchlight"}
+    };
+
+    // ------------------ GRID DETECTION ------------------
+    const QHash<QString, QString> gridWords {
+        {"grid", "grid"},
+        {"gridded", "grid"},
+        {"nogrid", "no-grid"},
+        {"no_grid", "no-grid"},
+        {"gridless", "no-grid"},
+        {"40", "40-grid"},
+        {"50", "50-grid"}
+    };
+
+    // ------------------ EXTENSION DETECTION ------------------
+    const QHash<QString, QString> extensionTags {
+        {"jpg",  "image"},
+        {"jpeg", "image"},
+        {"png",  "image"},
+        {"gif",  "animated"},
+        {"mp4",  "video"},
+        {"m4v",  "video"},
+        {"webm", "video"},
+        {"pdf",  "document"},
+        {"docx", "document"},
+        {"url",  "document"}
+    };
+
+    // ------------------ CREATOR PREFIXES ------------------
+    const QList<QPair<QString, QString>> creators {
+        {"ADMaps", "ADMaps"},
+        {"ChibbinGrove", "ChibbinGrove"},
+        {"Czepeku", "Czepeku"},
+        {"Domille", "Domille"},
+        {"EightfoldPaper", "EightfoldPaper"},
+        {"Elsewhere", "ElsewhereCompany"},
+        {"FableKit", "FableKit"},
+        {"Fantasy Atlas", "FantasyAtlas"},
+        {"GoAdventure", "GoAdventure"},
+        {"Loke", "LokeBattleMats"},
+        {"MapGuffin", "MapGuffin"},
+        {"Seafoot", "SeafootGames"},
+        {"Sirinkman", "Sirinkman"},
+        {"Spellarena", "Spellarena"},
+        {"Tehox", "TehoxMaps"},
+        {"Tom Cartos", "TomCartos"}
+    };
+
+    class TagFilterProxyModel : public QSortFilterProxyModel
+    {
+    public:
+        QStringList getRequiredTags();
+        void setRequiredTags(const QStringList& tags);
+        void clearRequiredTags();
+
+    protected:
+        virtual bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const override;
+        bool listContainsAllTags(const QStringList& list) const;
+
+    private:
+        QStringList _requiredTags;
+    };
+
 };
 
 Q_DECLARE_METATYPE(MapManagerDialog::MapFileMetadata);
