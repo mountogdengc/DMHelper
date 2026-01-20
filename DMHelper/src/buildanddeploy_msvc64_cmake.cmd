e2a799fa1f8538c60809762299b923fad8118430
DM Helper <44725987+dm-helper@users.noreply.github.com>
2026-01-20 12:53:21 +0100

    Updates around cmake build

--- DMHelper/src/buildanddeploy_msvc64_cmake.cmd
+++ DMHelper/src/buildanddeploy_msvc64_cmake.cmd
@@ -12,7 +12,9 @@ set QT_VERSION=6.10.1
 set QT_INSTALLER_VERSION=4.6
 set MSVC_VERSION=2022
 set SEVENZIP_APP=C:\Program Files\7-Zip\7z
-set PATH=%QT_DIR%\%QT_VERSION%\msvc%MSVC_VERSION%_64\bin;%QT_DIR%\Tools\QtInstallerFramework\%QT_INSTALLER_VERSION%\bin;%QT_DIR%\Tools\QtCreator\bin;%QT_DIR%\Tools\QtCreator\bin\jom;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+set "PATH=%QT_DIR%\%QT_VERSION%\msvc%MSVC_VERSION%_64\bin;%QT_DIR%\Tools\QtInstallerFramework\%QT_INSTALLER_VERSION%\bin;%QT_DIR%\Tools\QtCreator\bin;%QT_DIR%\Tools\CMake_64\bin;%PATH%"
+set
+echo %PATH%
 
 rem Uncomment the following line to skip actually building the SW
 rem goto skip_build
@@ -35,12 +37,31 @@ rename ..\bin64\packages\com.dmhelper.app\meta\installscript64.qs installscript.
 
 cd ..
 
+@CALL :Message "Preparing Visual Studio"
+REM Find latest Visual Studio installation
+for /f "usebackq delims=" %%i in (`
+  "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" ^
+    -latest ^
+    -products * ^
+    -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
+    -property installationPath
+`) do set "VS_INSTALL_DIR=%%i"
+
+if not defined VS_INSTALL_DIR (
+    echo ERROR: Visual Studio with C++ tools not found
+    exit /b 1
+)
+
+call "%VS_INSTALL_DIR%\Common7\Tools\VsDevCmd.bat" -arch=x64
+rem call "%VS_INSTALL_DIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
+rem call vcvarsall.bat x64
+
+
 @CALL :Message "Compiling DMHelper"
 
 rmdir /s /q .\build-64_bit-release
 mkdir build-64_bit-release
 cd build-64_bit-release
-call vcvarsall.bat x64
 @echo on
 cmake -S ..\src\ -B .\ -G "Visual Studio 17 2022"
 cmake --build .\ --config Release
@@ -53,7 +74,8 @@ cd build-64_bit-release
 :build_done
 @CALL :Message "Copy resource content"
 xcopy .\release\DMHelper.exe ..\bin64\packages\com.dmhelper.app\data\
-xcopy %QT_DIR%\%QT_VERSION%\msvc%MSVC_VERSION%_64\bin\Qt6Xml.dll ..\bin64\packages\com.dmhelper.app\data\
+xcopy "%QT_DIR%\%QT_VERSION%\msvc%MSVC_VERSION%_64\bin\Qt6Xml.dll" "..\bin64\packages\com.dmhelper.app\data\"
+xcopy $env:QT_DIR\$env:QT_VERSION\msvc$env:MSVC_VERSION_64\bin\Qt6Xml.dll ..\bin64\packages\com.dmhelper.app\data\
 xcopy ..\src\bin-win64\* ..\bin64\packages\com.dmhelper.app\data\*
 xcopy /s ..\src\bestiary\* ..\bin64\packages\com.dmhelper.app\data\resources\*
 xcopy /s ..\src\doc\* ..\bin64\packages\com.dmhelper.app\data\doc\*
