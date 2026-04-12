58b4def3950a3e10facf657492109c55b6fbd1f9
DM Helper <44725987+dm-helper@users.noreply.github.com>
2026-04-12 14:44:43 +0200

    Superficial updates

--- DMHelper/src/layertokensdarkeneffect.h
+++ DMHelper/src/layertokensdarkeneffect.h
@@ -2,36 +2,15 @@
 #define LAYERTOKENSDARKENEFFECT_H
 
 #include <QGraphicsEffect>
-#include <QPainter>
 
 class LayerTokensDarkenEffect : public QGraphicsEffect
 {
 public:
-    explicit LayerTokensDarkenEffect(QObject *parent = nullptr) : QGraphicsEffect(parent) {}
+    explicit LayerTokensDarkenEffect(QObject *parent = nullptr);
 
 protected:
-    QRectF boundingRectFor(const QRectF &sourceRect) const override {
-        return sourceRect;
-    }
-
-    void draw(QPainter *painter) override {
-        QPoint offset;
-        QPixmap pixmap = sourcePixmap(Qt::DeviceCoordinates, &offset);
-        if (pixmap.isNull())
-            return;
-
-        // Paint the dark overlay only onto opaque pixels of the source
-        QPainter overlayPainter(&pixmap);
-        overlayPainter.setCompositionMode(QPainter::CompositionMode_SourceAtop);
-        overlayPainter.fillRect(pixmap.rect(), QColor(0, 0, 0, 200));
-        overlayPainter.end();
-
-        // Draw in device coords so the dirty region matches exactly
-        QTransform restoreTransform = painter->worldTransform();
-        painter->setWorldTransform(QTransform());
-        painter->drawPixmap(offset, pixmap);
-        painter->setWorldTransform(restoreTransform);
-    }
+    QRectF boundingRectFor(const QRectF &sourceRect) const override;
+    void draw(QPainter *painter) override;
 };
 
 #endif // LAYERTOKENSDARKENEFFECT_H
